93e271f8c2022514948cc2371dfd5e474d4909ab
Xiangfei <yuxf@yahoo-inc.com>
2015-07-26 22:22:59 -0700

    wechat validate

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ wechat.List = require('./lib/list');
 wechat.Event = require('./lib/events');
 
 module.exports = wechat;
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  var echoString = request.query.echostr;
+  console.log("echoString = ", echoString);
+  response.send(echoString);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
