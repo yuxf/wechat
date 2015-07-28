566cc45cbd64537803bf358bfab58bee95a65072
Xiangfei <yuxf@yahoo-inc.com>
2015-07-27 20:53:57 -0700

    reverse unnecessary change

--- index.js
+++ index.js
@@ -6,30 +6,22 @@ wechat.Event = require('./lib/events');
 
 module.exports = wechat;
 
+// ===========
+
 TOKEN = "feiyuitravel";
 
+/*
 var config = {
   token: TOKEN,
   appid: 'wx5d34f7a9cb77d517',
   encodingAESKey: '5FLR9cvgrcA4XdaUA1KffSAlZ058bqXHVCigcEzcxij'
 };
+*/
 
 var express = require('express');
-// var crypto = require('crypto')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-/*
-app.get('/', function(request, response) {
-  if(checkSignature(request)) {
-    var echoString = request.query.echostr;
-    response.send(echoString);
-  }
-  else {
-    response.send("Hello");
-  }
-});
-*/
 app.use(express.query());
 app.use('/', wechat(TOKEN, function (req, res, next) {
   console.log(req.weixin);
@@ -44,26 +36,3 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-function checkSignature(req)
-{
-  var signature = req.query.signature;
-  var timestamp = req.query.timestamp;
-  var nonce = req.query.nonce;  
-  
-  var token = TOKEN;
-  var tmpArr = [token, timestamp, nonce];
-  console.log(tmpArr.sort().join(""));
-  var sha1Str = sha1(tmpArr.sort().join(""));
-  console.log(sha1Str + " ... " + signature);
-
-  if( sha1Str.localeCompare(signature) == 0 ){
-    return true;
-  }else{
-    return false;
-  }
-}
-/*
-function sha1(input){
-  return crypto.createHash('sha1').update(input).digest('hex')
-}
-*/
