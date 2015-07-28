306cb9c887c916fac33735b6829a1e2cf07b9e80
Xiangfei <yuxf@yahoo-inc.com>
2015-07-27 21:07:23 -0700

    change to encoded auth

--- index.js
+++ index.js
@@ -10,20 +10,18 @@ module.exports = wechat;
 
 TOKEN = "feiyuitravel";
 
-/*
 var config = {
   token: TOKEN,
   appid: 'wx5d34f7a9cb77d517',
   encodingAESKey: '5FLR9cvgrcA4XdaUA1KffSAlZ058bqXHVCigcEzcxij'
 };
-*/
 
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.query());
-app.use('/', wechat(TOKEN, function (req, res, next) {
+app.use('/', wechat(config, function (req, res, next) {
   console.log(req.weixin);
   res.reply({
       content: 'hehe',
