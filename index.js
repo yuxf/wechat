0761456c4fb253f71a9d0bef176e732576889c67
Xiangfei <yuxf@yahoo-inc.com>
2015-07-27 21:29:04 -0700

    add handler

--- index.js
+++ index.js
@@ -19,16 +19,39 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.query());
-app.use('/', wechat(config, function (req, res, next) {
-  console.log(req.weixin);
-  res.reply({
-      content: 'hehe',
-      type: 'text'
-    });
-}));
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.use('/', (config, wechat.text(function (req, res, next) {
+  console.log(req.weixin);
+  res.reply([
+		  {
+		    title: '你来我家接我吧',
+		    description: '这是女神与高富帅之间的对话',
+		    picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+		    url: 'http://nodeapi.cloudfoundry.com/'
+		  }
+	   ]);
+}).image(function (message, req, res, next) {
+
+}).voice(function (message, req, res, next) {
+
+}).video(function (message, req, res, next) {
+
+}).shortvideo(function (message, req, res, next) {
+
+}).location(function (message, req, res, next) {
+
+}).link(function (message, req, res, next) {
+
+}).event(function (message, req, res, next) {
+
+}).device_text(function (message, req, res, next) {
+
+}).device_event(function (message, req, res, next) {
+
+})
+));
 
